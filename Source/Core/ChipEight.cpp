c7b4828046e3d385cb13b67f96498166755d04ad
Mitch Andrews <rastaninja77@gmail.com>
2022-03-06 05:58:22 -0500

    🏓 Renders Properly + Cleanup

--- Source/Core/ChipEight.cpp
+++ Source/Core/ChipEight.cpp
@@ -13,6 +13,8 @@ void ChipEight::Initialize()
     StackPointer = 0;
 
     memset(&Data[0], 0, kMemorySize);
+    // uncomment when input goes in
+    //memset(&Key[0], 0, 16);
     memset(&GfxBuffer[0], 0, (64 * 32));
     memset(&Registers[0], 0, 16);
 
@@ -43,6 +45,11 @@ void ChipEight::EmulateCycle()
 {
     OpCode = Data[ProgramCounter] << 8 | Data[ProgramCounter + 1];
 
+    ProgramCounter += 2;
+
+    unsigned short x = (OpCode & 0x0F00) >> 8;
+    unsigned short y = (OpCode & 0x00F0) >> 4;
+
     switch (OpCode & 0xF000)
     {
         case 0x0000:
@@ -51,7 +58,6 @@ void ChipEight::EmulateCycle()
             {
                 case 0x0000:
                     // Clear Screen
-                    ProgramCounter += 2;
                     break;
                 case 0x000E:
                     // Subroutine return
@@ -65,73 +71,64 @@ void ChipEight::EmulateCycle()
         case 0x1000: // Jump
         {
             ProgramCounter = (OpCode & 0x0FFF);
+
             break;
         }
 
         case 0x3000: // Skip next instruction if Vx = kk.
         {
-            unsigned char index = (OpCode & 0x0F00) >> 8;
-
-            if (Registers[index] == (OpCode & 0x00FF))
+            if (Registers[x] == (OpCode & 0x00FF))
             {
                 ProgramCounter += 2;
             }
-
-            ProgramCounter += 2;
 
             break;
         }
 
         case 0x4000: // Skip next instruction if Vx != kk.
         {
-            unsigned char index = (OpCode & 0x0F00) >> 8;
-
-            if (Registers[index] != (OpCode & 0x00FF))
+            if (Registers[x] != (OpCode & 0x00FF))
             {
                 ProgramCounter += 2;
             }
 
-            ProgramCounter += 2;
-
-            break;
-        }
-            
-        case 0xA000: //Set I = nnn. - The value of register I is set to nnn.
-        {
-            RegisterIndex = OpCode & 0x0FFF;
-            ProgramCounter += 2;
             break;
         }
 
         case 0x6000: // Set Register - 6xkk - Vx = kk.
         {
-            unsigned char index = (OpCode & 0x0F00)>>8;
-            Registers[index] = OpCode & 0x00FF;
-
-            ProgramCounter += 2;
+            Registers[x] = OpCode & 0x00FF;
             break;
         }
 
         case 0x7000: // 7xkk - Set Vx = Vx + kk.
         {
-            unsigned char index = (OpCode & 0x0F00) >> 8;
-            Registers[index] += OpCode & 0x00FF;
-
-            ProgramCounter += 2;
+            Registers[x] += OpCode & 0x00FF;
             break;
         }
 
         case 0x8000:
         {
-            unsigned char x = Registers[(OpCode & 0x0F00) >> 8];
-            unsigned char y = Registers[(OpCode & 0x00F0) >> 4];
-
             switch (OpCode & 0x000F)
             {
                 case 0x0002:
                 {
-                    Registers[(OpCode & 0x0F00) >> 8] = (x & y);
-                    ProgramCounter += 2;
+                    Registers[x] &= Registers[y];
+                    break;
+                }
+
+                case 0x0004: // Set Vx = Vx + Vy, set VF = carry.
+                {
+                    unsigned short val = Registers[x] + Registers[y];
+                    Registers[0xF] = 0;
+                    if (val > 255)
+                    {
+                        Registers[0xF] = 1;
+                        val -= 256;
+                    }
+
+                    Registers[x] = val;
+
                     break;
                 }
 
@@ -144,10 +141,23 @@ void ChipEight::EmulateCycle()
             break;
         }
 
+        case 0xA000: //Set I = nnn. - The value of register I is set to nnn.
+        {
+            RegisterIndex = OpCode & 0x0FFF;
+            break;
+        }
+
+        case 0xC000:
+        {
+            unsigned char n = (OpCode & 0x000F);
+
+            Registers[x] = floor((rand() % 0xFF) & n);
+
+            break;
+        }
+
         case 0xD000: // Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision.
         {
-            unsigned char x = Registers[(OpCode & 0x0F00) >> 8];
-            unsigned char y = Registers[(OpCode & 0x00F0) >> 4];
             unsigned char n = (OpCode & 0x000F);
             unsigned short pixel = 0;
 
@@ -159,7 +169,7 @@ void ChipEight::EmulateCycle()
                 {
                     if ((pixel & (0x80 >> xPos)) != 0)
                     {
-                        const unsigned short index = (x + xPos + ((y + yPos) * 64));
+                        const unsigned short index = (Registers[x] + xPos + ((Registers[y] + yPos) * 64));
                         if (GfxBuffer[index] == 1)
                         {
                             Registers[0xF] = 1;
@@ -169,11 +179,108 @@ void ChipEight::EmulateCycle()
                     }
                 }
             }
-            ProgramCounter += 2;
             break;
         }
+
+        case 0xE000:
+        {
+            switch (OpCode & 0x00FF)
+            {
+                case 0x009E:
+                {
+                    if (Key[Registers[x]] != 0)
+                    {
+                        ProgramCounter += 2;
+                    }
+                    break;
+                }
+                case 0x00A1:
+                {
+                    unsigned char key = Registers[x];
+
+                    if (Key[key] == 0)
+                    {
+                        ProgramCounter += 2;
+                    }
+                    break;
+                }
+                default:
+                    std::cout << "Unknown OpCode: " << std::to_string(OpCode);
+                    break;
+            }
+            break;
+        }
+
+        case 0xF000:
+        {
+            switch (OpCode & 0x00FF)
+            {
+                case 0x000A: // Wait for a key press, store the value of the key in Vx.
+                {
+                    bool keyPress = false;
+                    for (int i = 0; i < 16; ++i)
+                    {
+                        if (Key[i] != 0)
+                        {
+                            Registers[x] = i;
+                            keyPress = true;
+                        }
+                    }
+                    if (!keyPress)
+                    {
+                        ProgramCounter -= 2;
+                    }
+                    break;
+                }
+
+                case 0x0018:
+                {
+                    SoundTimer = Registers[x];
+                    break;
+                }
+
+                case 0x0029: // Set I = location of sprite for digit Vx.
+                {
+                    RegisterIndex = Registers[x] * 5;
+                    break;
+                }
+
+                case 0x0033: // Store BCD representation of Vx in memory locations I, I+1, and I+2.
+                {
+                    unsigned char xVal = Registers[x];
+
+                    for (int i = 3; i > 0; --i)
+                    {
+                        Data[RegisterIndex + i - 1] = xVal % 10;
+                        xVal /= 10;
+                    }
+
+                    break;
+                }
+
+                case 0x0065: // Read registers V0 through Vx from memory starting at location I.
+                {
+                    for (int i = 0; i <= x; ++i)
+                    {
+                        Registers[i] = Data[RegisterIndex + i];
+                    }
+                    break;
+                }
+
+                default:
+                    std::cout << "Unknown OpCode: " << std::to_string(OpCode);
+                    break;
+            }
+            break;
+        }
+
         default:
             std::cout << "Unknown OpCode: " << std::to_string(OpCode);
             break;
+    }
+
+    if (SoundTimer > 0)
+    {
+        SoundTimer--;
     }
 }
